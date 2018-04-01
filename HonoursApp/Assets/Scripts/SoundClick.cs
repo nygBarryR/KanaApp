967ce205cca79df07f53076945a72d40aed69c4c
nygBarryR <32172899+nygBarryR@users.noreply.github.com>
2018-04-01 02:24:52 +0100

    Panel Meanings / Animations / Sound Added
    - All sound has been added for the letter pronunciation for both Hiragana & Katakana, along with required scripts and sound files.
    -Animations have been completed for Hiragana Panels / Katakana to follow
    - Word examples and Kana meaning added to all the panels.

---
+++ HonoursApp/Assets/Scripts/SoundClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class SoundClick : MonoBehaviour
+{
+
+    public AudioClip lettersound;
+
+    private Button SoundButton { get { return GetComponent<Button>(); } }
+    private AudioSource Source { get { return GetComponent<AudioSource>(); } }
+
+
+    void Start()
+    {
+
+        gameObject.AddComponent<AudioSource>();
+        Source.clip = lettersound;
+        Source.playOnAwake = false;
+
+        SoundButton.onClick.AddListener(() => PlaySound());
+    }
+
+    void PlaySound()
+    {
+        Source.PlayOneShot(lettersound);
+    }
+}
